132b3a56ecf5261c631d8c699b3513cfe80cecd5
Michał Lach <michal.lach@jit.team>
2020-01-14 14:23:25 +0100

    Abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,20 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbers = new NumbersCollection([-1, 10, 2, 3, 5, 12, 1]);
-const chars = new CharactersCollection("dupaxXxadadzzzgfa");
-const sorter = new Sorter(chars);
+// const numbers = new NumbersCollection([-1, 10, 2, 3, 5, 12, 1]);
+// const chars = new CharactersCollection("dupaxXxadadzzzgfa");
+// const sorter = new Sorter(chars);
 
-sorter.sort();
-console.log(sorter.collection.data);
+// sorter.sort();
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(300);
+linkedList.add(-2);
+linkedList.add(20);
+
+// sorter.sort();
+linkedList.print();
